2767161bb3814642c3a183fee39c3b35a77e4b11
E3TU <eetux22@protonmail.com>
2022-12-25 18:42:08 +0200

    Added 4th option to check for rkhunter updates

--- main.sh
+++ main.sh
@@ -5,30 +5,31 @@ if [[ $UID != 0 ]]; then
     echo "sudo $0 $*"
     exit 1
 fi
-echo "Do you want to run (1)Rkhunter (2)Chkrootkit (3)Both"
+echo "Do you want to run (1)Rkhunter (2)Check for rkhunter updates (3)Chkrootkit (4)Both"
 read input
-echo "test $input"
 if [ $input == 1 ]
 then
-    echo "Checking for rkhunter updates..."
-    sudo rkhunter --update
-    echo "Running propupd(this might take a while)..."
-    sudo rkhunter --propupd
     echo "Scanning for rootkits..."
     sudo rkhunter --checkall --sk
 fi
 
 if [ $input == 2 ]
 then
-    echo "Running chkrootkit..."
-    sudo chkrootkit
+    echo "Checking for rkhunter updates..."
+    sudo rkhunter --update
+    echo "Running propupd(this might take a while)..."
+    sudo rkhunter --propupd
 fi
 
 if [ $input == 3 ]
 then
-    echo "Running both chkrootkit and rkhunter(includes update checking)"
-    sudo rkhunter --update
-    sudo rkhunter --propupd
+    echo "Running chkrootkit..."
+    sudo chkrootkit
+fi
+
+if [ $input == 4 ]
+then
+    echo "Running both chkrootkit and rkhunter"
     sudo rkhunter --checkall --sk
     sudo chkrootkit
 fi
