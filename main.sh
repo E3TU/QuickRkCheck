b6963848bff99cb696739dd17a1284ab2f2710c5
E3TU <99178022+E3TU@users.noreply.github.com>
2023-02-12 18:39:29 +0200

    Cleaning code

--- main.sh
+++ main.sh
@@ -9,35 +9,36 @@ do
         echo "sudo $0 $*"
         exit 1
     fi
-    echo "Do you want to run (1)Rkhunter (2)Check for rkhunter updates (3)Chkrootkit (4)Both (5)Quit"
-    read input
-    if [ $input == 1 ]
-    then
-        echo "Scanning for rootkits..."
-        sudo rkhunter --checkall --sk
-    fi
-
-    if [ $input == 2 ]
+    echo "Do you want to run (1)Rkhunter (2)Chkrootkit (3)Both (4)Quit"
+    read -r input
+    if [ "$input" == 1 ]
     then
         echo "Checking for rkhunter updates..."
         sudo rkhunter --update
         echo "Running propupd(this might take a while)..."
         sudo rkhunter --propupd
+        echo "Scanning for rootkits..."
+        sudo rkhunter --checkall --sk
     fi
 
-    if [ $input == 3 ]
+    if [ "$input" == 2 ]
     then
         echo "Running chkrootkit..."
         sudo chkrootkit
     fi
 
-    if [ $input == 4 ]
+    if [ "$input" == 3 ]
     then
         echo "Running both chkrootkit and rkhunter"
+        echo "Checking for rkhunter updates..."
+        sudo rkhunter --update
+        echo "Running propupd(this might take a while)..."
+        sudo rkhunter --propupd
         sudo rkhunter --checkall --sk
         sudo chkrootkit
     fi
-    if [ $input == 5 ]
+
+    if [ "$input" == 4 ]
     then
         continueUse=2;
     fi
